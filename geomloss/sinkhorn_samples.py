1fab7e6eb89e20346ffbea227be47a9bc4610fed
narsyn <94863191+narsyn@users.noreply.github.com>
2022-05-18 10:24:58 +0200

    optionaly remove epsilon scaling in sinkhorn loop

--- geomloss/sinkhorn_samples.py
+++ geomloss/sinkhorn_samples.py
@@ -361,6 +361,7 @@ def sinkhorn_online(
     debias=True,
     potentials=False,
     a_init=0,
+    const_iterations=0
     **kwargs,
     
 ):
@@ -399,6 +400,10 @@ def sinkhorn_online(
     diameter, eps, eps_list, rho = scaling_parameters(
         x, y, p, blur, reach, diameter, scaling
     )
+    if(const_iterations>0){
+        eps_list = np.arange(const_iterations)
+        eps_list.fill(eps)
+    }
 
     f_aa, g_bb, g_ab, f_ba = sinkhorn_loop(
         softmin,
