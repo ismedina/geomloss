Fix dual error formula

geomloss/sinkhorn_divergence.py
@@ -652,11 +652,11 @@ def sinkhorn_loop(
     # g_ab_prev.detach() TODO: needed? It is already detached, right?
     # IM: One can check that the error in the coupling _before_ computing the 
     # potential in the next line is given by the formula
-    # sinkhorn_error = \sum_i b_i*|1 - g_i^k / g_i^{k+1}|
-    # where k denotes the iteration. Thus, by saving g_ab_prev
+    # sinkhorn_error = \sum_i b_i*|1 - v_i^k / v_i^{k+1}|
+    # where k denotes the iteration and v the scaling factor. Thus, by saving g_ab_prev
     # we can compute an estimate of the error in the following line. 
     g_ab = damping * softmin(eps, C_yx, (a_log + f_ba / eps))
-    sinkhorn_error = torch.sum(torch.exp(b_log)*torch.abs(1 - g_ab_prev/g_ab))
+    sinkhorn_error = torch.sum(torch.exp(b_log)*torch.abs(1 - torch.exp((g_ab_prev - g_ab)/eps)))
 
     if debias:
         return sinkhorn_error, (f_aa, g_bb, g_ab, f_ba)